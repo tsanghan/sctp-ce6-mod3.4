eb628f0eac9c7ef71da6cd1a387be19714f15d1f
Wong Tsang Han <tsanghan@gmail.com>
2024-08-22 20:06:19 +0800

    added README.md

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const express = require('express')
-const app = express()
+// before rum
+//npm install express
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// to run
+// npm start
 
-app.listen(3000)
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const OS = require('os');
+const ENV = 'DEV';
+
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello from Node!';
+  res.send(msg);
+});
+
+app.get('/test', (req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello from /test Node!';
+  res.send(msg);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
