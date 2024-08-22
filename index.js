3513de6fbd7977ababb7547cbe69932287e597e2
Wong Tsang Han <tsanghan@gmail.com>
2024-08-22 20:19:35 +0800

    rever back to original code

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = process.env.APP_PORT || 8080;
 const HOST = '0.0.0.0';
 const OS = require('os');
 const ENV = 'DEV';
